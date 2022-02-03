e746ce580ea6c811b918fd0629ed21454ac64c4e
tacosontitan <jkdavis1691@gmail.com>
2022-02-03 08:54:44 -0600

    Added region tags to neural node.

--- models/NeuralNode.cs
+++ models/NeuralNode.cs
@@ -1,6 +1,17 @@
 namespace NeuralNetworkTest;
 public class NeuralNode {
+
+    #region Properties
+
     public double Value { get; set; }
     public List<NeuralNode> ConnectedNodes { get; private set; } = new List<NeuralNode>();
+
+    #endregion
+
+    #region Constructor
+
     public NeuralNode(double value) => Value = value;
+
+    #endregion
+    
 }
