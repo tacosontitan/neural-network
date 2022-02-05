315289171caff1768a60afdc0f6be6c1678b0b70
tacosontitan <jkdavis1691@gmail.com>
2022-02-05 09:57:30 -0600

    Removed code in program file.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
-﻿using NeuralNetworkTest;
-var randomGenerator = new Random();
-for (int i = 0; i < 10; i++) {
-    var node = new NeuralNode(randomGenerator.NextDouble());
-    Console.WriteLine(node.Value);
-}
+﻿
